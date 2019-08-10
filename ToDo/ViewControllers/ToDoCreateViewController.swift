297bbe3451958fcaa04f6d8b8b1b82de929e4939
Jaemyeong Jin <jaemyeong@me.com>
2019-08-10 15:54:59 +0900

    Core Data Context 수정
    Signed-off-by: Jaemyeong Jin <jaemyeong@me.com>

--- ToDo/ViewControllers/ToDoCreateViewController.swift
+++ ToDo/ViewControllers/ToDoCreateViewController.swift
@@ -2,52 +2,56 @@ import UIKit
 import CoreData
 
 class ToDoCreateViewController: UIViewController {
-
-    //MARK: CoreData Properties
-    var managedContext: NSManagedObjectContext!
+    
     var todo: Todo?
 
-    //MARK: 키보드 이슈 찾아보기
-    //MARK: Outlets
     @IBOutlet weak var textView: UITextView!
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         self.textView.becomeFirstResponder()
-
+        
         if let todo = todo {
-            textView.text = todo.title
             textView.text = todo.title
             segmentedControl.selectedSegmentIndex = Int(todo.priotity)
         }
-
     }
+    
     @IBAction func save(_ sender: Any) {
         guard let title = textView.text, !title.isEmpty else {
             return
         }
 
-
         if let todo = self.todo {
             todo.title = title
             todo.priotity = Int16(segmentedControl.selectedSegmentIndex)
         } else {
-            let todo = Todo(context: managedContext)
-            todo.title = title
-            todo.priotity = Int16(segmentedControl.selectedSegmentIndex)
-            todo.date = Date()
-        }
-
-        do {
-            try managedContext.save()
-            self.navigationController?.popViewController(animated: true)
-            textView.resignFirstResponder()
-        } catch {
-            print("Error saving todo: \(error)")
+            let delegate: AppDelegate! = UIApplication.shared.delegate as? AppDelegate
+            assert(delegate != nil)
+            
+            let priority = Int16(self.segmentedControl.selectedSegmentIndex)
+            
+            delegate.persistentContainer.performBackgroundTask { (context) in
+                let todo = Todo(context: context)
+                todo.title = title
+                todo.priotity = priority
+                todo.date = Date()
+                
+                do {
+                    try context.save()
+                    
+                    DispatchQueue.main.async {
+                        self.navigationController?.popViewController(animated: true)
+                    }
+                } catch {
+                    print("Error saving todo: \(error)")
+                }
+            }
         }
     }
 }
--- ToDo/ViewControllers/ToDoListViewController.swift
+++ ToDo/ViewControllers/ToDoListViewController.swift
@@ -9,7 +9,6 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
 
     // MARK: - CoreData Properties
     var resultController: NSFetchedResultsController<Todo>!
-    let coreDataStack = CoreDataStack()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,36 +16,38 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
         // 테이블뷰의 프로토콜 위임자 설정
         self.todoListTableView.delegate = self
         self.todoListTableView.dataSource = self
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
 
-        // Request
-        let request: NSFetchRequest<Todo> = Todo.fetchRequest()
+        let request: NSFetchRequest = Todo.fetchRequest()
         let sortDescriptors = NSSortDescriptor(key: "date", ascending: true)
-
-        // Initialize resultController
+        
+        let delegate: AppDelegate! = UIApplication.shared.delegate as? AppDelegate
+        assert(delegate != nil)
+        
         request.sortDescriptors = [sortDescriptors]
         self.resultController = NSFetchedResultsController(fetchRequest: request,
-                                                      managedObjectContext: coreDataStack.managedContext,
+                                                      managedObjectContext: delegate.persistentContainer.viewContext,
                                                       sectionNameKeyPath: nil,
-                                                      cacheName: nil
-        )
-
+                                                      cacheName: nil)
         self.resultController.delegate = self
 
-        // Fetch
         do {
             try resultController.performFetch()
         } catch {
             print("Perform fetch error: \(error)")
         }
+        
+        NotificationCenter.default.addObserver(forName: .NSManagedObjectContextObjectsDidChange, object: nil, queue: nil) { (notification) in
+            
+            delegate.persistentContainer.viewContext.mergeChanges(fromContextDidSave: notification)
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return resultController.sections?.count ?? 0
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        self.todoListTableView.reloadData()
-    }
-
-
-    // MARK: - TableViewDataSource's methods implement
-    // 테이블뷰의 셀 등록과 셀 속성 정의
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return resultController.sections?[section].numberOfObjects ?? 0
     }
@@ -66,20 +67,25 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
 
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let action = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
-            // TODO: Delete todo
             let todo = self.resultController.object(at: indexPath)
-            self.resultController.managedObjectContext.delete(todo)
-            do {
-                try self.resultController.managedObjectContext.save()
-                completion(true)
-            } catch {
-                print("delete failed: \(error)")
-                completion(false)
+            
+            let delegate: AppDelegate! = UIApplication.shared.delegate as? AppDelegate
+            assert(delegate != nil)
+            
+            delegate.persistentContainer.performBackgroundTask { (context) in
+                context.delete(todo)
+                
+                do {
+                    try context.save()
+                    completion(true)
+                } catch {
+                    print("delete failed: \(error)")
+                    completion(false)
+                }
             }
-
         }
+        
         action.title = "Delete"
-        action.backgroundColor = .red
 
         return UISwipeActionsConfiguration(actions: [action])
     }
@@ -87,17 +93,22 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
 
         let action = UIContextualAction(style: .destructive, title: "Done") { (action, view, completion) in
-            // TODO: Done todo
             let todo = self.resultController.object(at: indexPath)
-            self.resultController.managedObjectContext.delete(todo)
-            do {
-                try self.resultController.managedObjectContext.save()
-                completion(true)
-            } catch {
-                print("delete failed: \(error)")
-                completion(false)
+            
+            let delegate: AppDelegate! = UIApplication.shared.delegate as? AppDelegate
+            assert(delegate != nil)
+            
+            delegate.persistentContainer.performBackgroundTask { (context) in
+                context.delete(todo)
+                
+                do {
+                    try context.save()
+                    completion(true)
+                } catch {
+                    print("delete failed: \(error)")
+                    completion(false)
+                }
             }
-
         }
 
         action.title = "Done"
@@ -106,23 +117,12 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
         return UISwipeActionsConfiguration(actions: [action])
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "showAddTodo", sender: todoListTableView.cellForRow(at: indexPath))
-    }
-
-
     // MARK: - Navigaiton
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let _ = sender as? UIBarButtonItem, let vc = segue.destination as? ToDoCreateViewController {
-            vc.managedContext = resultController.managedObjectContext
-        }
-
-        if let cell = sender as? UITableViewCell, let vc = segue.destination as? ToDoCreateViewController {
-            vc.managedContext = resultController.managedObjectContext
-            if let indexPath = todoListTableView.indexPath(for: cell) {
-                let todo = resultController.object(at: indexPath)
-                vc.todo = todo
+        if let vc = segue.destination as? ToDoCreateViewController {
+            if let indexPath = todoListTableView.indexPathForSelectedRow {
+                vc.todo = resultController.object(at: indexPath)
             }
         }
     }
